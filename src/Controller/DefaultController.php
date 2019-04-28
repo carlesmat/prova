48be78cf06ca48e1510ec0fcdbccf76830c72333
Carles Matallana <carlesmat@gmail.com>
2019-04-28 20:38:59 +0200

    initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/DefaultController.php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController
+{
+/**
+* @Route("/index")
+*/
+
+    public function index()
+    {
+	return new Response('<html><body>Hola que tal.</body></html>'
+        );
+    }
+}
+?>
